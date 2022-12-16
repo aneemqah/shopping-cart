5f21d38d2392e25d30ff6ac7fb3d758620e2409e
Meena Haque <aneemqah@gmail.com>
2022-12-16 17:30:12 +0000

    feat: add react-bootstrap to display products

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,5 +1,24 @@
+import { Row, Col } from 'react-bootstrap';
+
 function Store() {
-  return <h1>Welcome to the store!</h1>;
+  return (
+    <>
+      <h1>Welcome to the store!</h1>
+      {/* xs - show only 1 column */}
+      {/* md - show only 3 column */}
+      <Row xs={1} md={3} className="g-4">
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Store;

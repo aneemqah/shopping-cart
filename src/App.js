e81a2821afcc970ea2b31613fc033f86fc89f911
Meena Haque <aneemqah@gmail.com>
2022-12-16 17:16:45 +0000

    feat: add route for store page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Store from './pages/Store';
 
 function App() {
   return (
     <Container>
       <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }

54bda0518c8b7da4234932145102e70f46389b20
Meena Haque <aneemqah@gmail.com>
2022-12-17 21:36:16 +0000

    feat: center h1 text

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -4,7 +4,9 @@ import { products } from '../productsStore';
 function Store() {
   return (
     <>
-      <h1>Welcome to the store!</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the store!
+      </h1>
       {/* xs - show only 1 column */}
       {/* md - show only 3 column */}
       <Row xs={1} md={3} className="g-4">

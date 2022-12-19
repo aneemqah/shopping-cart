cea9467cc1f2230b134ad9aaf458ddc6183ecddc
Meena Haque <aneemqah@gmail.com>
2022-12-19 10:57:40 +0000

    feat: display modal for when user clicks on Cart Items button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,30 @@
-import { Navbar, Button } from 'react-bootstrap';
+import { Navbar, Button, Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
 function Nav() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+  const handleShow = () => {
+    setShow(true);
+  };
+
   return (
-    <Navbar expand="sm">
-      <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
-      <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
-      <Button>Cart 0 Items</Button>
-    </Navbar>
+    <>
+      <Navbar expand="sm">
+        <Navbar.Brand href="/">Ecommerce</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end"></Navbar.Collapse>
+        <Button onClick={handleShow}>Cart 0 Items</Button>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </Modal>
+    </>
   );
 }
 
